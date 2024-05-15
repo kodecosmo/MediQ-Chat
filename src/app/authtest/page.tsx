1e20bd19176a82fe6e2656d2ec42e8194f5a071d
kaviru <hapuarachchikaviru@gmail.com>
2024-05-15 12:11:30 +0530

    Name added. Need some config in production

--- src/app/authtest/page.tsx
+++ src/app/authtest/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 
-import { useUser, SignInButton } from "@clerk/nextjs";
+import { useUser, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 
 
@@ -15,7 +15,7 @@ export default function Auth() {
         return (
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <h1>Sign in to access this page</h1>
-                <SignInButton mode="modal" fallbackRedirectUrl="/community">
+                <SignInButton mode="modal" fallbackRedirectUrl="/">
                     <button className="mt-5 border border-secondary bg-white px-4 py-1.5 text-base hover:text-white hover:bg-black">
                         <span>Sign in</span>
                     </button>
@@ -26,6 +26,11 @@ export default function Auth() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             Hello {user.fullName}
+            <SignOutButton redirectUrl='/authtest'>
+                <button className="mt-5 border border-secondary bg-white px-4 py-1.5 text-base hover:text-white hover:bg-black">
+                    <span>Sign out</span>
+                </button>
+            </SignOutButton>
         </main>
     );
 }
