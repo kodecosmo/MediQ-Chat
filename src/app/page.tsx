db4d61e46676faaee97826d7a55fbf0a0c479fbb
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-05-16 12:44:39 +0530

    chore: Add sidebar component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
 import { userAgent } from "next/server";
 import { Navbar } from "@/Components/navbar";
+import { Sidebar } from "@/Components/sidebar";
+
 
 export default function Home() {
-
   const { user } = useUser();
 
   return (
     <>
-      <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          
-        Hello {user && user.fullName}
+      {/* <Navbar /> */}
 
-        
+      <main className="">
+        {/* Hello {user && user.fullName} */}
+
+        {/* sidebar */}
+        <section>
+          <Sidebar />
+        </section>
       </main>
     </>
   );
